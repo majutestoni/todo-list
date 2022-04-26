72bb9fa6081301053466770ae866eccacd6e8667
Maju Testoni <majuliatestoni@gmail.com>
2022-04-26 20:36:22 -0300

    tentando checkbox

--- src/components/TelaPrincipal.vue
+++ src/components/TelaPrincipal.vue
@@ -7,10 +7,8 @@
       <ul class="lista">
         <li v-for="(todo, index) in todoList" :key="index">
           <div class="fleg">
-            <input type="checkbox" class="concluir" name="" id="" />
-           
-            <p>{{ todo.description }}</p>
-           
+            <input type="checkbox" class="concluir" name="" id="" />           
+            <p>{{ todo.description }}</p>           
             <input v-show="editID === todo.id" type="text" />
           </div>
           <div class="icones">
@@ -109,7 +107,7 @@ li {
   margin-top: 10px;
   background: rgba(243, 224, 253, 0.747);
   border-radius: 6px;
-  font-size: 19px;
+  font-size: 16px;
   padding: 6px;
   display: flex;
   justify-content: space-between;
@@ -140,46 +138,26 @@ li {
   font-size: 18px;
 }
 
-.concluir {
-  position: relative;
-  width: 1.5em;
-  height: 1.5em;
-  color: black;
-  border: 1px solid gray;
-  border-radius: 4px;
-  appearance: none;
-  outline: 0;
-  cursor: pointer;
-  transition: 175ms cubic-bezier(0.1, 0.1, 0.25, 1);
-}
-.concluir::before {
-  position: absolute;
-  content: '';
-  display: block;
-  top: 2px;
-  left: 7px;
-  width: 8px;
-  height: 14px;
-  border-style: solid;
-  border-color: white;
-  border-width: 0 2px 2px 0;
-  transform: rotate(45deg);
-  opacity: 0;
+.concluir{
+ margin-top: 3px;
+height: 18px;
+width: 18px;
+background-color: rgb(238, 238, 238);
+border-radius: 50px;
+cursor: pointer;
 }
 
-.concluir:checked {
-  color: white;
-  border-color: green;
-  background: green;
-}
-input[type="checkbox"]::before {
-  opacity: 1;
+.concluir:hover{
+  background-color: aqua;
 }
 
-label::before {
-  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+.fleg:hover input ~ .concluir{
+  background-color: aqua;
 }
 
+.fleg input:checked ~ .concluir{
+  background-color: rgb(255, 0, 0);
+}
 
 label {
   position: relative;
@@ -189,14 +167,6 @@ label {
   padding: 0 0.25em 0;
   user-select: none;
 }
-label::before {
-  position: absolute;
-  content: attr(data-content);
-  color: lightgray;
-  clip-path: polygon(0 0, 0 0, 0% 100%, 0 100%);
-  text-decoration: line-through;
-  text-decoration-thickness: 3px;
-  text-decoration-color: black;
-  transition: clip-path 200ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
-}
+
+
 </style>>
